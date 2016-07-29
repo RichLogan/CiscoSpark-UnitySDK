589ca18e244c8d71802be7669baa1df4fca47ee8
Rich Logan <accounts@richlogan.co.uk>
2016-07-29 12:06:36 +0100

    Renaming AuthenticationString to AuthenticationToken

--- Spark SDK/Assets/Cisco/Spark SDK/Request.cs
+++ Spark SDK/Assets/Cisco/Spark SDK/Request.cs
@@ -4,12 +4,12 @@ using UnityEngine.Networking;
 public class Request : MonoBehaviour {
 	// Publically Set Variables
 	public string BaseUrl = "https://api.ciscospark.com/v1";
-	public string AuthenticationString = "";
+	public string AuthenticationToken = "";
 
 	public UnityWebRequest Generate(string Resource, string RequestType) {
 		// Setup Headers
 		UnityWebRequest www = new UnityWebRequest (BaseUrl + "/" + Resource);
-		www.SetRequestHeader ("Authorization", "Bearer " + AuthenticationString);
+		www.SetRequestHeader ("Authorization", "Bearer " + AuthenticationToken);
 		www.SetRequestHeader ("Content-type", "application/json; charset=utf-8");
 		www.method = RequestType;
 		www.downloadHandler = new DownloadHandlerBuffer();
