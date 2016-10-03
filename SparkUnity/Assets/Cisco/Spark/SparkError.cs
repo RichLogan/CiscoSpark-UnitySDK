f58a37c3d7f61f23700f2051f167a9a395e29617
Rich Logan <accounts@richlogan.co.uk>
2016-10-03 15:24:27 +0100

    Cleanup + Membership Change
    Sorry for the messy commit, but essentially this achieves:
    
    - Cleanup and renaming of some folders / structure to make it better
    - Implements the ability to report back on Spark side errors using `SparkMessage` and `SparkError` via the new `err` callbacks. 
    - Implements NodeJS callbacks (`err = > {}, res => {}`) for the `Membership` class. Will be implementing this for all objects.

---
+++ SparkUnity/Assets/Cisco/Spark/SparkError.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Cisco.Spark {
+	public class SparkError {
+
+		public string Description;
+
+		public SparkError(Dictionary<string, object> data) {
+			Description = data ["description"] as string;
+		}
+	}
+}
