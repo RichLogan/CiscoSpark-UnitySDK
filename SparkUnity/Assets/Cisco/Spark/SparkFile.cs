427ccbb8b9381ef14f78f993863788da8da64e84
Rich Logan <accounts@richlogan.co.uk>
2016-10-28 10:24:07 +0100

    Style code correctly

--- SparkUnity/Assets/Cisco/Spark/SparkFile.cs
+++ SparkUnity/Assets/Cisco/Spark/SparkFile.cs
@@ -3,7 +3,6 @@ using UnityEngine.Networking;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using MiniJSON;
 
 namespace Cisco.Spark {
 	public class SparkFile  {
@@ -12,7 +11,7 @@ namespace Cisco.Spark {
 		public byte[] Data { get; set;}
 
 		// Return helper
-		public Type returnType;
+		public Type ReturnType;
 
 		/// <summary>
 		/// Initializes a new <see cref="Cisco.Spark.SparkFile"/> instance from a given Spark File ID.
@@ -22,8 +21,9 @@ namespace Cisco.Spark {
 			Id = id;
 		}
 
-		public SparkFile(byte[] data) {
-			
+		public SparkFile(string filename, byte[] data) {
+			Filename = filename;
+			Data = data;
 		}
 
 		/// <summary>
@@ -32,31 +32,31 @@ namespace Cisco.Spark {
 		/// <param name="callback">File data as byte array</param>
 		/// <param name="forceBytes">Stops intelligent conversion of file array (e.g png -> Texture) and forces returning of a raw byte array. </param>
 		public IEnumerator Download(Action<object> callback, bool forceBytes = false) {
-			Request manager = GameObject.FindObjectOfType<Request> ();
+			var manager = GameObject.FindObjectOfType<Request> ();
 			using (UnityWebRequest www = manager.Generate ("contents/" + Id, UnityWebRequest.kHttpVerbGET)) {
 				yield return www.Send ();
 				if (www.isError) {
 					Debug.LogError ("Failed to Download File: " + www.error);
 				} else {
-					Dictionary<string, string> headers = www.GetResponseHeaders ();
+					var headers = www.GetResponseHeaders ();
 					Data = www.downloadHandler.data;
 					Filename = headers ["Content-Disposition"].Split ('"')[1];
 					string fileType = Filename.Split ('.')[1];
 					if (fileType.Equals ("png") || fileType.Equals ("jpg")) {
 						// Downloaded File is a supported image
-						Texture2D texture = new Texture2D (2,2);
+						var texture = new Texture2D (2,2);
 						texture.LoadImage (Data);
-						returnType = typeof(UnityEngine.Texture2D);
+						ReturnType = typeof(UnityEngine.Texture2D);
 						callback (texture);
 					}
 					else if (forceBytes) {
 						// Asked for bytes
-						returnType = typeof(System.Byte);
+						ReturnType = typeof(Byte);
 						callback (Data);
 					} else {
 						// TODO: Support more file types
 						// No supported file type, returning bytes
-						returnType = typeof(System.Byte);
+						ReturnType = typeof(Byte);
 						callback (Data);
 					}
 				}
@@ -64,13 +64,13 @@ namespace Cisco.Spark {
 		}
 
 		public IEnumerator GetHeaders(Action<Dictionary<string, string>> callback) {
-			Request manager = GameObject.FindObjectOfType<Request> ();
+			var manager = GameObject.FindObjectOfType<Request> ();
 			using (UnityWebRequest www = manager.Generate ("contents/" + Id, UnityWebRequest.kHttpVerbHEAD)) {
 				yield return www.Send ();
 				if (www.isError) {
 					Debug.LogError ("Failed to Download File Headers: " + www.error);
 				} else {
-					Dictionary<string, string> headers = www.GetResponseHeaders ();
+					var headers = www.GetResponseHeaders ();
 					Filename = headers ["Content-Disposition"].Split ('"')[1];
 					callback (headers);
 				}
