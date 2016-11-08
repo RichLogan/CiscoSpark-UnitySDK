f169b1884fc20a28e488d8df2540e23a98229c04
Rich Logan <accounts@richlogan.co.uk>
2016-11-08 12:43:52 +0000

    Add better Spark File support
    - Extension and Return Type set earlier (GetHeaders())
    - OBJ file support
    - Initial tests

--- SparkUnity/Assets/Cisco/Spark/SparkFile.cs
+++ SparkUnity/Assets/Cisco/Spark/SparkFile.cs
@@ -5,9 +5,11 @@ using System.Collections;
 using System.Collections.Generic;
 
 namespace Cisco.Spark {
-	public class SparkFile  {
+
+	public class SparkFile {
 		public string Id { get; set;}
 		public string Filename { get; set;}
+		public string Extension { get; set;}
 		public byte[] Data { get; set;}
 
 		// Return helper
@@ -27,10 +29,10 @@ namespace Cisco.Spark {
 		}
 
 		/// <summary>
-		/// Download the File itself
+		/// Download the File data.
 		/// </summary>
-		/// <param name="callback">File data as byte array</param>
-		/// <param name="forceBytes">Stops intelligent conversion of file array (e.g png -> Texture) and forces returning of a raw byte array. </param>
+		/// <param name="callback">File data as Unity specific object or bytes.</param>
+		/// <param name="forceBytes">Stops intelligent conversion of file array (e.g png -> Texture) and forces returning of a raw byte array.</param>
 		public IEnumerator Download(Action<object> callback, bool forceBytes = false) {
 			var manager = Request.Instance;
 			using (UnityWebRequest www = manager.Generate ("contents/" + Id, UnityWebRequest.kHttpVerbGET)) {
@@ -38,24 +40,40 @@ namespace Cisco.Spark {
 				if (www.isError) {
 					Debug.LogError ("Failed to Download File: " + www.error);
 				} else {
+					// Get Headers.
 					var headers = www.GetResponseHeaders ();
+					// Get data as bytes.
 					Data = www.downloadHandler.data;
+					// Get filename from Content-Disposition header.
 					Filename = headers ["Content-Disposition"].Split ('"')[1];
-					string fileType = Filename.Split ('.')[1];
-					if (fileType.Equals ("png") || fileType.Equals ("jpg")) {
-						// Downloaded File is a supported image
-						var texture = new Texture2D (2,2);
-						texture.LoadImage (Data);
-						ReturnType = typeof(UnityEngine.Texture2D);
-						callback (texture);
-					}
-					else if (forceBytes) {
-						// Asked for bytes
-						ReturnType = typeof(Byte);
+					// Parse Extension.
+					Extension = Filename.Substring(Filename.LastIndexOf ('.') + 1).ToLower ();
+					// Figure Unity-native ReturnType.
+					SetReturnType ();
+
+					// Convert data to ReturnType.
+					if (forceBytes) {
+						// Specifically asked for bytes.
 						callback (Data);
+					} else if (ReturnType == typeof(Texture)) {
+						// Downloaded File is a supported image.
+						var texture = new Texture2D (2, 2);
+						texture.LoadImage (Data);
+						callback (texture);
+					} else if (ReturnType == typeof(GameObject)) {
+						// File should return a GameObject.
+						if (Extension.Equals ("obj")) {
+							// File is an Obj.
+							var downloadedObject = new GameObject ();
+							var objImport = downloadedObject.AddComponent<ObjLoad> ();
+							var dataAsString = System.Text.Encoding.UTF8.GetString (Data);
+							objImport.SetGeometryData (dataAsString);
+							callback (downloadedObject);
+						}
 					} else {
-						// TODO: Support more file types
-						// No supported file type, returning bytes
+						// TODO: Support more file types.
+						// Unsupported file type, returning bytes.
+						Debug.LogWarning(Extension + " is unsupported, returning bytes.");
 						ReturnType = typeof(Byte);
 						callback (Data);
 					}
@@ -72,8 +90,24 @@ namespace Cisco.Spark {
 				} else {
 					var headers = www.GetResponseHeaders ();
 					Filename = headers ["Content-Disposition"].Split ('"')[1];
+					Extension = Filename.Substring(Filename.LastIndexOf ('.') + 1).ToLower ();
+					SetReturnType ();
 					callback (headers);
 				}
+			}
+		}
+
+		void SetReturnType() {
+			// Supported Return Types
+			var models = new List<string> {"obj"};
+			var images = new List<string> {"psd", "tiff", "jpg", "tga", "png", "gif", "bmp", "iff", "pict", "exr", "hdr"};
+
+			if (images.Contains (Extension)) {
+				// Image -> Texture
+				ReturnType = typeof(Texture);
+			} else if (models.Contains (Extension)) {
+				// 3D Model -> Mesh
+				ReturnType = typeof(GameObject);
 			}
 		}
 	}	
