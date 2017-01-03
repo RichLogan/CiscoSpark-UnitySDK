79f17d9572d724effd0d06b1ee5be36059ea3451
Rich Logan <accounts@richlogan.co.uk>
2017-01-03 16:32:24 +0000

    Format/Style SparkResources.cs

--- SparkUnity/Assets/Cisco/Spark/SparkResources.cs
+++ SparkUnity/Assets/Cisco/Spark/SparkResources.cs
@@ -3,54 +3,59 @@ using UnityEngine;
 using MiniJSON;
 using System.IO;
 
-namespace Cisco.Spark {
+namespace Cisco.Spark
+{
 
-	public enum SparkType {
-		Membership,
-		Room,
-		Person,
-		Team,
-		TeamMembership,
-		Message,
-		Webhook
-	}
+    public enum SparkType
+    {
+        Membership,
+        Room,
+        Person,
+        Team,
+        TeamMembership,
+        Message,
+        Webhook
+    }
 
-	public class SparkResources : MonoBehaviour {
+    public class SparkResources : MonoBehaviour
+    {
 
-		/// <summary>
-		/// Singleton Instance.
-		/// </summary>
-		public static SparkResources Instance;
+        /// <summary>
+        /// Singleton Instance.
+        /// </summary>
+        public static SparkResources Instance;
 
-		/// <summary>
-		/// Mapping of SparkType to URL endpoints.
-		/// </summary>
-		public Dictionary<SparkType, string> UrlEndpoints;
+        /// <summary>
+        /// Mapping of SparkType to URL endpoints.
+        /// </summary>
+        public Dictionary<SparkType, string> UrlEndpoints;
 
-		public Dictionary<string, object> ApiConstraints;
-	
-		void Awake() {
-			// Singleton.
-			Instance = this;
+        public Dictionary<string, object> ApiConstraints;
 
-			// Spark Type URL Endpoints.
-			UrlEndpoints = new Dictionary<SparkType, string> {
-				{ SparkType.Membership, "memberships" },
-				{ SparkType.Room, "rooms" },
-				{ SparkType.Person, "people" },
-				{ SparkType.Team, "teams" },
-				{ SparkType.TeamMembership, "team/memberships" },
-				{ SparkType.Message, "messages" },
-				{ SparkType.Webhook, "webhooks" },
-			};
+        void Awake()
+        {
+            // Singleton.
+            Instance = this;
 
-			// Load API Constraints from resource file.
-			string contents;
-			using (var streamReader = new StreamReader("Assets/Cisco/Spark/ApiConstraints.json")) {
-				contents = streamReader.ReadToEnd();
-			}
-			ApiConstraints = Json.Deserialize(contents) as Dictionary<string, object>;
-			ApiConstraints = ApiConstraints["apiConstraints"] as Dictionary<string, object>;
-		}
-	}
+            // Spark Type URL Endpoints.
+            UrlEndpoints = new Dictionary<SparkType, string> {
+                { SparkType.Membership, "memberships" },
+                { SparkType.Room, "rooms" },
+                { SparkType.Person, "people" },
+                { SparkType.Team, "teams" },
+                { SparkType.TeamMembership, "team/memberships" },
+                { SparkType.Message, "messages" },
+                { SparkType.Webhook, "webhooks" },
+            };
+
+            // Load API Constraints from resource file.
+            string contents;
+            using (var streamReader = new StreamReader("Assets/Cisco/Spark/ApiConstraints.json"))
+            {
+                contents = streamReader.ReadToEnd();
+            }
+            ApiConstraints = Json.Deserialize(contents) as Dictionary<string, object>;
+            ApiConstraints = ApiConstraints["apiConstraints"] as Dictionary<string, object>;
+        }
+    }
 }
