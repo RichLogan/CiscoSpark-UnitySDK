acc17b97fa8f51995af26ffa070e058c5f7917bb
Rich Logan <rich@richlogan.co.uk>
2017-01-06 17:42:27 +0000

    Rewrite TeamMembership
    - Initial tests
    - Api Constraints added

---
+++ SparkUnity/Assets/Cisco/Spark/TeamMembership.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cisco.Spark
+{
+    /// <summary>
+    /// TeamMembership representats a <see cref="Person"/>'s relationship to a <see cref="Team"/>.
+    /// </summary>
+    public class TeamMembership : SparkObject
+    {
+        /// <summary>
+        /// The <see cref="Team"/> the Membership belongs to.
+        /// </summary>
+        public Team Team { get; set; }
+
+        /// <summary>
+        /// The <see cref="Person"/> the Membership belongs to.
+        /// </summary>
+        public Person Person { get; set; }
+
+        /// <summary>
+        /// True if the <see cref="Person"/> is a moderator of the Team.
+        /// </summary>
+        public bool IsModerator { get; set; }
+
+        /// <summary>
+        /// <see cref="SparkType"/> this <see cref="SparkObject"/> implementation represents.
+        /// </summary>
+        internal override SparkType SparkType
+        {
+            get { return SparkType.TeamMembership; }
+        }
+
+        /// <summary>
+        /// Constructor to build representation of existing Spark-side TeamMembership.
+        /// Use Load() to populate rest of properties from Spark.
+        /// </summary>
+        /// <param name="id">Spark UID of the TeamMembership.</param>
+        public TeamMembership(string id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Cisco.Spark.TeamMembership"/> class.
+        /// </summary>
+        /// <param name="team">The Team of the TeamMembership.</param>
+        /// <param name="person">The person belonging to the TeamMembership.</param>
+        /// <param name="isModerator">True if this member will be a moderator.</param>
+        public TeamMembership(Team team, Person person, bool isModerator = false)
+        {
+            Team = team;
+            Person = person;
+            IsModerator = isModerator;
+        }
+
+        /// <summary>
+        /// Returns a dictionary representation of the object.
+        /// </summary>
+        /// <returns>The Dictionary.</returns>
+        /// <param name="fields">A specific list of fields to serialise.</param>
+        protected override Dictionary<string, object> ToDict(List<string> fields = null)
+        {
+            // Serialise to dictionary.
+            var data = base.ToDict();
+            data["teamId"] = Team.Id;
+            data["personId"] = Person.Id;
+            data["isModerator"] = IsModerator;
+            return CleanDict(data, fields);
+        }
+
+        /// <summary>
+        /// Populates the TeamMembership with data from Spark.
+        /// </summary>
+        /// <param name="data">Dictionary of TeamMembership data.</param>
+        protected override void LoadDict(Dictionary<string, object> data)
+        {
+            base.LoadDict(data);
+            var teamId = data["teamId"] as string;
+            Team = new Team(teamId);
+            var personId = data["personId"] as string;
+            Person = new Person(personId);
+            IsModerator = (bool)data["isModerator"];
+        }
+
+        /// <summary>
+        /// Lists all TeamMemberships matching the given criteria.
+        /// </summary>
+        /// <param name="error">Error from Spark, if any.</param>
+        /// <param name="results">List of TeamMemberships.</param>
+        /// <param name="room">The Room to show TeamMemberships for.</param>
+        /// <param name="person">The person to show TeamMemberships for.</param>
+        /// <param name="max">The maximum number of TeamMemberships to return.</param>
+        /// <returns></returns>
+		public static IEnumerator ListTeamMemberships(Action<SparkMessage> error, Action<List<TeamMembership>> results, Team team = null, Person person = null, int max = 0)
+        {
+            var constraints = new Dictionary<string, string>();
+            if (team != null)
+            {
+                constraints.Add("teamId", team.Id);
+            }
+            else if (person != null)
+            {
+                constraints.Add("personId", person.Id);
+            }
+
+            if (max > 0)
+            {
+                constraints.Add("max", max.ToString());
+            }
+
+            var listObjects = ListObjects<TeamMembership>(constraints, SparkType.TeamMembership, error, results);
+            yield return Request.Instance.StartCoroutine(listObjects);
+        }
+    }
+}
---
+++ SparkUnity/Assets/Cisco/Spark/Tests/TestTeamMembership.cs
@@ -0,0 +1,180 @@
+﻿using UnityEngine;
+using Cisco.Spark;
+
+public class TestTeamMembership : MonoBehaviour
+{
+    [TooltipAttribute("This must NOT be the person creating the Team")]
+    public string testPersonid = "";
+
+    Person person;
+    TeamMembership membership;
+    Team testTeam;
+
+    bool start = false;
+
+    void Update()
+    {
+        // We need to use Person.AuthenticatedUser so have to wait for it to be ready.
+        if (Request.Instance.SetupComplete && !start)
+        {
+            TestStart();
+            start = true;
+        }
+    }
+
+    void TestStart()
+    {
+        // Check people.
+        if (testPersonid == Person.AuthenticatedUser.Id)
+        {
+            Fail("Test person cannot be the authenticated user.");
+        }
+
+        // Need a Person and a Team first!
+        person = new Person(testPersonid);
+        testTeam = new Team();
+        testTeam.Name = "Cisco Spark Unity Team Test";
+        // Commit Test Team.
+        StartCoroutine(testTeam.Commit(error =>
+        {
+            Fail(error.Message);
+        }, result =>
+        {
+            CreateMembership();
+        }));
+
+    }
+
+    void CreateMembership()
+    {
+        // Got new Team, now create membership.
+        var newMembership = new TeamMembership(testTeam, person);
+
+        // Commit to Spark.
+        StartCoroutine(newMembership.Commit(error =>
+        {
+            // Failed!
+            Fail(error.Message);
+        }, commited =>
+        {
+            // Seems to have worked, but we don't know for sure yet.
+            membership = newMembership;
+            GetMembership();
+        }));
+    }
+
+    void GetMembership()
+    {
+        var retrieve = new TeamMembership(membership.Id);
+        StartCoroutine(retrieve.Load(error =>
+        {
+            // Failed!
+            Fail(error.Message);
+        }, success =>
+        {
+            if (retrieve.Team.Id == membership.Team.Id)
+            {
+                // Create is now known to have passed here.
+                Debug.Log("Create Team Membership Passed!");
+                // Get Membership just passed.
+                Debug.Log("Get Team Membership Passed!");
+                // Move on.
+                UpdateMembership();
+            }
+        }));
+    }
+
+    void UpdateMembership()
+    {
+        // Let's set moderator to true.
+        if (membership.IsModerator == true)
+        {
+            Fail("Moderator should start false");
+        }
+        else
+        {
+            membership.IsModerator = true;
+            StartCoroutine(membership.Commit(error =>
+            {
+                // Failed.
+                Fail(error.Message);
+            }, success =>
+            {
+                // Looks like it's worked, let's check.
+                var checkingModerator = new TeamMembership(membership.Id);
+                StartCoroutine(checkingModerator.Load(error =>
+                {
+                    Fail(error.Message);
+                }, checkSuccess =>
+                {
+                    if (checkingModerator.IsModerator)
+                    {
+                        // Now we know Update passed for sure.
+                        Debug.Log("Update Team Membership Passed!");
+                        ListMemberships();
+                    }
+                }));
+            }));
+        }
+    }
+
+    void ListMemberships()
+    {
+        // List all memberships and see if our new one is there.
+        StartCoroutine(TeamMembership.ListTeamMemberships(error =>
+        {
+            // Failed.
+            Fail(error.Message);
+        }, results =>
+        {
+            foreach (var mem in results)
+            {
+                if (mem.Id == membership.Id)
+                {
+                    // Found it!
+                    Debug.Log("List Team Membership Memberships Passed");
+                    DeleteMembership();
+                }
+            }
+        }, testTeam));
+    }
+
+    void DeleteMembership()
+    {
+        var oldMembershipId = string.Copy(membership.Id);
+        StartCoroutine(membership.Delete(error =>
+        {
+            Fail(error.Message);
+        }, success =>
+        {
+            // Double check (Expect get to know fail).
+            var checkDelete = new TeamMembership(oldMembershipId);
+            StartCoroutine(checkDelete.Load(error =>
+            {
+                if (error.Message.Equals("Failed to get membership."))
+                {
+                    Debug.Log("Delete TeamMembership Passed!");
+                    TestEnd();
+                }
+            }, getSuccess =>
+            {
+                Fail("Should fail to Get deleted Membership");
+            }));
+        }));
+    }
+
+    void TestEnd()
+    {
+        // Delete the Team for cleanup.
+        StartCoroutine(testTeam.Delete(error => Fail(error.Message), success =>
+        {
+            Debug.Log("Deleted the test Team");
+            Debug.Log("***TestTeamMembership Finished***");
+        }));
+    }
+
+    void Fail(string error)
+    {
+        throw new System.Exception("TeamMembership tests failed: " + error);
+    }
+}
