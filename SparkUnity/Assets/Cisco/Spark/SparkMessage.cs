2279442576c585dd3c38bbe4dd1c288740d7059a
Rich Logan <accounts@richlogan.co.uk>
2017-01-10 18:18:30 +0000

    Add Properties for SparkMessage.
    - Documentation
    - Format

--- SparkUnity/Assets/Cisco/Spark/SparkMessage.cs
+++ SparkUnity/Assets/Cisco/Spark/SparkMessage.cs
@@ -1,26 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
-namespace Cisco.Spark {
-	public class SparkMessage {
-		public string Message;
-		public IEnumerable<SparkError> Errors;
-		public string TrackingId;
-	
-		public SparkMessage (Dictionary<string, object> data) {
-			Message = (string) data ["message"];
-			Errors = new List<SparkError> ();
+namespace Cisco.Spark
+{
+    /// <summary>
+    /// A message Spark returns in response to an operation.
+    /// This is usually a wrapping of a SparkError.
+    /// </summary>
+    public class SparkMessage
+    {
+        /// <summary>
+        /// The specific message from Spark.
+        /// </summary>
+        public string Message { get; private set; }
 
-			object errors;
-			if (data.TryGetValue ("errors", out errors)) {
-				var listOfErrors = errors as List<object>;
-				var errorList = new List<SparkError> ();
-				foreach (var error in listOfErrors) {
-					errorList.Add (new SparkError(error as Dictionary<string, object>));
-				}
-				Errors = errorList;
-			}
-			TrackingId = (string) data ["trackingId"];
-		}
-	}
+        /// <summary>
+        /// A collection of one or more <see cref="SparkError"/>s.
+        /// </summary>
+        public IEnumerable<SparkError> Errors { get; private set; }
+
+        /// <summary>
+        /// Spark's internal tracking ID for this message.
+        /// </summary>
+        public string TrackingId { get; private set; }
+
+        /// <summary>
+        /// Creates a SparkMessage from a JSON Spark response.
+        /// </summary>
+        /// <param name="data">Dictionary of data about the SparkMessage.</param>
+        public SparkMessage(Dictionary<string, object> data)
+        {
+            Message = (string)data["message"];
+            Errors = new List<SparkError>();
+
+            object errors;
+            if (data.TryGetValue("errors", out errors))
+            {
+                var listOfErrors = errors as List<object>;
+                var errorList = new List<SparkError>();
+                foreach (var error in listOfErrors)
+                {
+                    errorList.Add(new SparkError(error as Dictionary<string, object>));
+                }
+                Errors = errorList;
+            }
+            TrackingId = (string)data["trackingId"];
+        }
+    }
 }
