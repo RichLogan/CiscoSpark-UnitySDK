1c078f9a19e05beccafb58a66a486ed5e8500033
Rich Logan <rich@richlogan.co.uk>
2017-02-09 12:49:39 +0000

    Add tests for Team functionality
    - CRUD
    - List

---
+++ SparkUnity/Assets/Cisco/Spark/Tests/TestCreateTeam.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Cisco.Spark;
+
+public class TestCreateTeam : MonoBehaviour
+{
+    Team team;
+
+    void Start()
+    {
+        Test();
+    }
+
+    void Test()
+    {
+        // Try and create team.
+        team = new Team();
+        team.Name = "Unity SDK Test Team";
+        StartCoroutine(team.Commit(error => {
+            IntegrationTest.Fail(error.Message);
+        }, success => {
+            TearDown();
+            IntegrationTest.Pass();
+        }));
+    }
+
+    void TearDown()
+    {
+        StartCoroutine(team.Delete(error => {
+            IntegrationTest.Fail("Failed to delete test Team: " + error.Message);
+        }, success => {
+            return;
+        }));
+    }
+}
---
+++ SparkUnity/Assets/Cisco/Spark/Tests/TestDeleteTeam.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Cisco.Spark;
+
+public class TestDeleteTeam : MonoBehaviour
+{
+    Team team;
+
+    void Start()
+    {
+        SetUp();
+    }
+
+    void SetUp()
+    {
+        team = new Team();
+        team.Name = "Unity SDK Test Room";
+        StartCoroutine(team.Commit(error =>
+        {
+            IntegrationTest.Fail("Failed to create example team: " + error.Message);
+        }, success =>
+        {
+            Test();
+        }));
+    }
+
+    void Test()
+    {
+        StartCoroutine(team.Delete(error =>
+        {
+            IntegrationTest.Fail(error.Message);
+        }, success =>
+        {
+            IntegrationTest.Pass();
+        }));
+    }
+}
