de72d395b43003bc43fdfd4ec8eb678d8cee702a
Rich Logan <accounts@richlogan.co.uk>
2017-02-09 13:04:21 +0000

    Build PR Branch
    Travis wasn't actually building the PR request branch (instead master)!

--- Travis/getSDK.sh
+++ Travis/getSDK.sh
@@ -5,5 +5,5 @@ mkdir TravisBuild
 cd TravisBuild
 echo "Attempting to fetch Cisco Spark SDK"
 mkdir -p Project/
-git clone --recursive https://github.com/RichLogan/CiscoSpark-UnitySDK.git Project
+git clone -b $TRAVIS_PULL_REQUEST_BRANCH --recursive https://github.com/RichLogan/CiscoSpark-UnitySDK.git Project
 echo "SDK Ready"
