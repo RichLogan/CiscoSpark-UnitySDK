26927c75122c952b32be789b557beddbadc01cf7
Rich Logan <rich@richlogan.co.uk>
2017-04-18 14:30:54 +0100

    Generated from ac280e7

--- search/all_f.js
+++ search/all_f.js
@@ -1,8 +1,10 @@
 var searchData=
 [
   ['target',['Target',['../class_cisco_1_1_spark_1_1_webhook.html#ad99e756a3354648e8b41872443af74e0',1,'Cisco::Spark::Webhook']]],
-  ['team',['Team',['../class_cisco_1_1_spark_1_1_team.html',1,'Cisco.Spark.Team'],['../class_cisco_1_1_spark_1_1_room.html#a64b293326491496268e24954ef15b9a9',1,'Cisco.Spark.Room.Team()'],['../class_cisco_1_1_spark_1_1_team_membership.html#a19df9fedd83a06fd4b5a6adf2342dce2',1,'Cisco.Spark.TeamMembership.Team()'],['../class_cisco_1_1_spark_1_1_team.html#ad1bbccc9462b94c0dcc81f41a2c680b5',1,'Cisco.Spark.Team.Team(string id)'],['../class_cisco_1_1_spark_1_1_team.html#a83de636e572b9b3bb622edd325efa302',1,'Cisco.Spark.Team.Team()']]],
-  ['teammembership',['TeamMembership',['../class_cisco_1_1_spark_1_1_team_membership.html',1,'Cisco.Spark.TeamMembership'],['../class_cisco_1_1_spark_1_1_team_membership.html#a866c9600d03ec4cf90b73c9a145aedf6',1,'Cisco.Spark.TeamMembership.TeamMembership(string id)'],['../class_cisco_1_1_spark_1_1_team_membership.html#a4d9b4cfa69077f02741f3490439d6027',1,'Cisco.Spark.TeamMembership.TeamMembership(Team team, Person person, bool isModerator=false)']]],
+  ['team',['Team',['../class_cisco_1_1_spark_1_1_room.html#a64b293326491496268e24954ef15b9a9',1,'Cisco.Spark.Room.Team()'],['../class_cisco_1_1_spark_1_1_team_membership.html#a19df9fedd83a06fd4b5a6adf2342dce2',1,'Cisco.Spark.TeamMembership.Team()'],['../class_cisco_1_1_spark_1_1_team.html#ad1bbccc9462b94c0dcc81f41a2c680b5',1,'Cisco.Spark.Team.Team(string id)'],['../class_cisco_1_1_spark_1_1_team.html#a83de636e572b9b3bb622edd325efa302',1,'Cisco.Spark.Team.Team()']]],
+  ['team',['Team',['../class_cisco_1_1_spark_1_1_team.html',1,'Cisco::Spark']]],
+  ['teammembership',['TeamMembership',['../class_cisco_1_1_spark_1_1_team_membership.html',1,'Cisco::Spark']]],
+  ['teammembership',['TeamMembership',['../class_cisco_1_1_spark_1_1_team_membership.html#a866c9600d03ec4cf90b73c9a145aedf6',1,'Cisco.Spark.TeamMembership.TeamMembership(string id)'],['../class_cisco_1_1_spark_1_1_team_membership.html#a4d9b4cfa69077f02741f3490439d6027',1,'Cisco.Spark.TeamMembership.TeamMembership(Team team, Person person, bool isModerator=false)']]],
   ['text',['Text',['../class_cisco_1_1_spark_1_1_message.html#a229fcdc9b85957975b86745fc5ff4616',1,'Cisco::Spark::Message']]],
   ['texture',['Texture',['../class_cisco_1_1_spark_1_1_avatar.html#a4b93a9405a62f8e66b388444cf1ecbd0',1,'Cisco::Spark::Avatar']]],
   ['title',['Title',['../class_cisco_1_1_spark_1_1_room.html#aa051a56f07269c0d16eccd3d8ba83626',1,'Cisco::Spark::Room']]],
